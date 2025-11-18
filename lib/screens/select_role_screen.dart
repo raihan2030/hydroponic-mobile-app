1a4f57fe2e6719687ba475208a35986c41330344
raihan2030 <mr799903@gmail.com>
2025-11-18 22:19:24 +0800

    ubah warna background halaman select role

--- lib/screens/select_role_screen.dart
+++ lib/screens/select_role_screen.dart
@@ -8,6 +8,7 @@ class SelectRoleScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text(
           'Pilih Posisi',
